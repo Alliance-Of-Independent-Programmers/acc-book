0f7ca46750465a9e103329d2df269dc92489328c
Artm-porjad <artm-porjad@mail.ru>
2021-10-22 18:21:52 +0300

    added frontend->backend communication 2

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, flash, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.post("/api/save_message")
+def save_message():
+    if request.method == 'POST':
+        email = request.form['email']
+        text = request.form['text']
+        print(email, text)
+    return "Ok"
